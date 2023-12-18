85e04ec0280c9a4e43c271b46f58544995da39f8
Noire <zackyboy135@gmail.com>
2023-12-17 17:48:16 -0700

    dear diary, for whom does the bell toll?
    bells and whistles! toot toot

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+		lanzaboote.url = "github:nix-community/lanzaboote";
 		nix-colors.url = "github:misterio77/nix-colors";
+
 		home-manager = {
 			url = "github:nix-community/home-manager/release-23.11";
 			inputs.nixpkgs.follows = "nixpkgs";
@@ -11,16 +13,23 @@
 	};
 
 
-	outputs = { self, home-manager, nixpkgs, ... } @inputs: let
+	outputs = { self, home-manager, nixpkgs, lanzaboote, ... } @inputs: let
 		system = "x86_64-linux";
 		lib = nixpkgs.lib;
-		pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; permittedInsecurePackages = [ "electron-25.9.0" ]; }; };
+		pkgs = import nixpkgs { 
+			inherit system; 
+			config = { 
+				allowUnfree = true; 
+				permittedInsecurePackages = [ "electron-25.9.0" ]; 
+			}; 
+		};
 	in {
 		nixosConfigurations = {
 			lastation = lib.nixosSystem {
 				specialArgs = { inherit inputs; };
 				modules = [ 
 					./configuration.nix
+					lanzaboote.nixosModules.lanzaboote
 				];
 			};
 		};
