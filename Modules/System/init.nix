3872e51e26a6fe43cb26bcea9199b50c07cb357c
Noire <zackyboy135@gmail.com>
2023-12-26 19:42:10 -0700

    dear diary, today i fixed my storage!
    turns out it might not have failed and the name went from
    /dev/sda3 to /dev/sdb3 and i have no clue why
    
    ...frick it, i guess.

--- Modules/System/init.nix
+++ Modules/System/init.nix
@@ -4,15 +4,15 @@
         # Directory initializers.
 		    ./Shell/init.nix
 		    ./Git/init.nix
+        ./storage.nix
   
         # WINE: WINE Is Now an Emulator.
 		    ./Utility/Wine/init.nix 
 
         # Build utilities.
 		    ./Utility/build.nix
-        # Don't ever use this.
-        ./storage.nix
 
+        # Don't ever use this.
 		    ./Utility/electron.nix
     ];
 }
--- Modules/System/storage.nix
+++ Modules/System/storage.nix
@@ -1,18 +1,26 @@
-{ pkgs, ... }:
+{ ... }:
 let
 	windowsFS = "Windows (C:)";
 	miscellaneousFS = "Miscellaneous (D:)";
 	devices = {
 		nvme="/dev/nvme0n1p4";
-		hdd="/dev/sdb3";
+		hdd="/dev/sda3";
 	};
 in {
 	fileSystems = {
 		"${windowsFS}" = {
 			label = windowsFS;
 			device = devices.nvme;
-			fsType = "ntfs";
-			
+			fsType = "lowntfs-3g";
+			options = [
+          "UUID=84D6B99BD6B98E44"
+          "uid=1000"
+          "gid=100"
+          "rw"
+          "user"
+          "exec"
+          "umask=000"
+      ];
 
 			# If this is enabled, say goodbye
 			# to Windows if something.. goes wrong.
@@ -22,11 +30,10 @@ in {
 			autoResize = false;
 			mountPoint = "/mnt/windows";
 		};
-		/*
 		"${miscellaneousFS}" = {
 			label = miscellaneousFS;
 			device = devices.hdd;
-			fsType = "ntfs";
+			fsType = "lowntfs-3g";
 
 			depends = [
 				"/mnt/windows"
@@ -37,7 +44,6 @@ in {
 			autoResize = false;
 			mountPoint = "/mnt/miscellaneous";
 		};
-		*/
 	};
 	
 }
