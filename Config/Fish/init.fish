88b56d7e6efc0846fd6aa4c1458af90e142402d5
Kishamo Smith <nowaaru@proton.me>
2023-12-27 23:16:32 -0700

    dear diary, today i developed some ambitions
    let's make a command helper utility thing for fish. yep!

--- Config/Fish/init.fish
+++ Config/Fish/init.fish
@@ -1,3 +1,42 @@
+# Helper functions.
+set description __fish_generate_help
+function __fish_generate_help \
+    -d $description \
+    -a command_name \
+    -a description \
+    -a flags
+
+
+end
+
+begin
+    set description 'Register a directory as an environment variable. Prepends \$CONFIG.'
+    function __fish_register_config \
+        -d $description \
+        -a config_id
+
+        argparse -n fish_register_config -X 1 "h/help=?" -- $argv
+        or return
+
+
+        if test -n $_flag_h
+            echo """
+      Fish Register Config
+      --------------------
+      
+      $description
+      
+      Usage
+      
+       __fish_register_config 
+    """
+            return
+        end
+
+        # set -gx CONFIG_$config_id $(pwd)
+    end
+end
+
 # Initialize auxiliary paths.
 set -Ux CONFIG $DIARY/Config
 set -Ux MODULES $DIARY/Modules
@@ -5,3 +44,6 @@ set -Ux MODULES $DIARY/Modules
 # Initialize abbreviations.
 abbr -a --position command mod "cd $MODULES"
 abbr -a --position command conf "cd $CONFIG"
+
+# Aliases.
+source $CONFIG/Fish/alias.fish
