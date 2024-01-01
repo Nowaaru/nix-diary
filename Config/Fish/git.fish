9f9124d1ae33b2bad462be3b371b2ed3b0eda68b
Nowaaru <nowaaru.proton.me>
2024-01-01 02:26:54 -0700

    fixup! dear diary, tonight i wanted to start programming more

--- Config/Fish/git.fish
+++ Config/Fish/git.fish
@@ -6,9 +6,7 @@ function pre_git -e fish_preexec
     if string match -qr (string escape (path resolve $DIARY)) (string escape (path resolve $REPO_DIR))
         if test (path basename $REPO_DIR) = (path basename $DIARY)
             if test -f $PRIVATE_NIX_PATH -a -e $PRIVATE_NIX_PATH
-                if git ls-files --error-unmatch $PRIVATE_NIX_PATH
-                    git add -Nf $PRIVATE_NIX_PATH
-                end
+                git add -Nf $PRIVATE_NIX_PATH
             end
         end
     end
