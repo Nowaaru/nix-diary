56306a397606338ee62d0a5c448a5277a976e8fb
Kishamo Smith <nowaaru@proton.me>
2024-01-01 01:18:50 -0700

    semi

--- Config/Hyprland/init.fish
+++ Config/Hyprland/init.fish
@@ -4,7 +4,7 @@ hyprpaper
 dunst
 
 # Set Hyprland environment variables.
-set -Ux QT_QPA_PLATFORM "wayland;xcb copyq"
+set -Ux QT_QPA_PLATFORM "wayland;xcb;copyq"
 
 # Initialize clipboard.
 copyq --start-server
