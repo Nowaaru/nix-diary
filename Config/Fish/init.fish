05cf82642cfaa368c0af38c1a7360dc0bfc76726
Kishamo Smith <nowaaru@proton.me>
2024-01-01 01:18:54 -0700

    add paths

--- Config/Fish/init.fish
+++ Config/Fish/init.fish
@@ -41,6 +41,9 @@ end
 set -Ux CONFIG $DIARY/Config
 set -Ux MODULES $DIARY/Modules
 
+set -Ux USER_MODULES $MODULES/User
+set -Ux SYSTEM_MODULES $MODULES/System
+
 # Initialize abbreviations.
 abbr -a --position command mod "cd $MODULES"
 abbr -a --position command conf "cd $CONFIG"
