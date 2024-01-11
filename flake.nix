018ba8872e859dd076bee72046f2bb859ef98961
Nowaaru <nowaaru@proton.me>
2024-01-10 21:52:12 -0700

    i hate vinegar

--- flake.nix
+++ flake.nix
@@ -4,14 +4,16 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 		nix-colors.url = "github:misterio77/nix-colors";
-    lanzaboote.url = "github:nix-community/lanzaboote";
-    nur.url = "github:nix-community/NUR";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+        lanzaboote.url = "github:nix-community/lanzaboote";
+        nur.url = "github:nix-community/NUR";
+        rust-overlay.url = "github:oxalica/rust-overlay";
 
 		home-manager = {
 			url = "github:nix-community/home-manager/master";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
+        
+
 	};
 
 
@@ -24,6 +26,32 @@
 				allowUnfree = true; 
 				permittedInsecurePackages = [ "electron-25.9.0" ]; 
 			};
+
+            /*
+            overlays = [
+                ( 
+                    { fetchurl, fetchpatch }:
+                        final: prev: {
+                            wine = prev.wine.overrideAttrs (old: {
+                                src = fetchTarball {
+                                    url = "https://dl.winehq.org/wine/source/9.0/wine-9.0-rc4.tar.xz";
+                                };
+                                patches =
+                                (old.patches or [ ])
+                                ++ [
+                                    # upstream issue: https://bugs.winehq.org/show_bug.cgi?id=55604
+                                    # Here are the currently applied patches for Roblox to run under WINE:
+                                    (fetchpatch {
+                                        name = "vinegar-wine-segrevert.patch";
+                                        url = "https://raw.githubusercontent.com/flathub/org.vinegarhq.Vinegar/8fc153c492542a522d6cc2dff7d1af0e030a529a/patches/wine/temp.patch";
+                                        hash = "sha256-AnEBBhB8leKP0xCSr6UsQK7CN0NDbwqhe326tJ9dDjc=";
+                                    })
+                                ];
+                            });
+                        }
+                )
+            ];
+            */
 		}; 
 	in {
     nixosConfigurations = {
@@ -42,6 +70,26 @@
 			modules = [ 
 				nur.nixosModules.nur
 				./home.nix 
+                ({
+                    nixpkgs.overlays = [ 
+                        (let
+                            neovim-nightly-overlay = import (builtins.fetchTarball {
+                                url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+                            });
+                        in 
+                        self: super:
+                        {
+                            inherit (neovim-nightly-overlay self super) neovim-nightly;
+                        })
+                    ];
+                    
+                    # programs.neovim = {
+                    #     enable = true;
+                    #     package = pkgs.neovim-nightly;
+                    #     extraLuaPackages = ps: [ ps.magick ];
+                    # };
+                    
+                })
 			];
 		};
 	};
