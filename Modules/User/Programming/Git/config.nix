35ba10098b1943882e3d5bbf28e8c487acd743b0
Nowaaru <nowaaru@proton.me>
2024-01-15 12:46:12 -0700

    fmt + warning

--- Modules/User/Programming/Git/config.nix
+++ Modules/User/Programming/Git/config.nix
@@ -1,29 +1,29 @@
-{ lib, ... }: 
+{ ... }: 
     let private = if builtins.pathExists ./private.nix then 
             import ./private.nix 
-        else { user = {}; };
+        else builtins.trace "warning: ${builtins.toString ./.}/private.nix does not exist" { user = {}; };
     in {
-    inherit (private) user;
+        inherit (private) user;
 
-    core = {
-        editor = "nvim";
-        pager = "delta";
-    };
+        core = {
+            editor = "nvim";
+            pager = "delta";
+        };
 
-    delta = {
-        features = [
-            "line-numbers"
-            "decorations"
-        ];
+        delta = {
+            features = [
+                "line-numbers"
+                "decorations"
+            ];
 
-        line-numbers = true;
-    };
+            line-numbers = true;
+        };
 
-    init = {
-        defaultBranch = "master";
-    };
+        init = {
+            defaultBranch = "master";
+        };
 
-    web.browser = "firefox";
-    gpg.format = "ssh";
-    push.autoSetupRemote = true;
+        web.browser = "firefox";
+        gpg.format = "ssh";
+        push.autoSetupRemote = true;
     }
