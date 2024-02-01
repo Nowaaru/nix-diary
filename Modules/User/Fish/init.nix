d345e92bccd73b329907f826b2a38ac56a832f81
Nowaaru <nowaaru@proton.me>
2024-01-31 17:28:54 -0700

    fmt

--- Modules/User/Fish/init.nix
+++ Modules/User/Fish/init.nix
@@ -1,15 +1,15 @@
-{ pkgs, ... }:
-{
-	programs.fish = {
-		enable = true;
-		shellInit = ''
-			set -Ux DIARY ~/.diary
-			source $DIARY/Config/Fish/init.fish
-		'';
-		interactiveShellInit = ''
-			set -U fish_greeting
-			ncneofetch
-			fish_vi_key_bindings
-		'';
-	};
+_: {
+  programs.fish = {
+    enable = true;
+    # useBabelfish = true;
+    shellInit = ''
+      set -Ux DIARY ~/.diary
+      source $DIARY/Config/Fish/init.fish
+    '';
+    interactiveShellInit = ''
+      set -U fish_greeting
+      ncneofetch
+      fish_vi_key_bindings
+    '';
+  };
 }
