fcacef2af2cc182b0936b49a1701d8a0dc98e03f
Nowaaru <nowaaru@proton.me>
2024-02-04 16:50:00 -0700

    fmt

--- Modules/System/init.nix
+++ Modules/System/init.nix
@@ -1,36 +1,39 @@
-{ pkgs, ... }:
-{
-    imports = [
-        # Directory initializers.
-		    ./Shell/init.nix
-		    ./Git/init.nix
-        ./storage.nix
-  
-        # WINE: WINE Is Now an Emulator.
-		    ./Utility/Wine/init.nix 
+{pkgs, ...}: {
+  imports = [
+    # Directory initializers.
+    ./Shell/init.nix
+    ./Git/init.nix
 
-        # Build utilities.
-		    ./Utility/build.nix
+    # WINE: WINE Is Now an Emulator.
+    ./Utility/Wine/init.nix
 
-        # Don't ever use this.
-		    ./Utility/electron.nix
+    # Build utilities.
+    ./Utility/build.nix
 
-        # Kitty terminal.
-		    ./Utility/kitty.nix
-        
-        # Various performance metrics.
-		    ./Utility/status.nix
-		    ./loopback.nix
-    ];
-    
-    # Dependencies and things.
-    environment.systemPackages = with pkgs; [
+    # Don't ever use this.
+    ./Utility/electron.nix
+
+    # Kitty terminal.
+    ./Utility/kitty.nix
+
+    # Various performance metrics.
+    ./Utility/status.nix
+    ./loopback.nix
+
+    # Stuff.
+    ./firefox.nix
+    ./storage.nix
+  ];
+
+  # Dependencies and things.
+  environment.systemPackages = with pkgs;
+    [
       libsForQt5.kio-admin
       dotnet-runtime
       grc
       fzf
       zip
       unzip
-    ] ++ [ home-manager ]; # do not remove home-manager.
+    ]
+    ++ [home-manager]; # do not remove home-manager.
 }
-
