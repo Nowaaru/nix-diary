9f0ea30b5f498f131ada0a515548a1924be22aea
Nowaaru <nowaaru@proton.me>
2024-02-04 16:53:12 -0700

    flatpak
    i probably will end up turning this off
    because i prefer the purity and declarativeness
    of Nix. flatpaks kinda encourage the complete
    opposite of that fwis

--- Core/configuration.nix
+++ Core/configuration.nix
@@ -228,6 +228,9 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Enable Flatpak sandboxing.
+  services.flatpak.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
