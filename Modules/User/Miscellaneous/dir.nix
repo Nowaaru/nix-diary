5ad2e8f39587691c4c051090ae0d41fd6f143a2c
Nowaaru <nowaaru@proton.me>
2024-02-04 16:48:56 -0700

    remove xdg horribleness

--- Modules/User/Miscellaneous/dir.nix
+++ Modules/User/Miscellaneous/dir.nix
@@ -1,18 +1,20 @@
 # Initialize XDG environment variables that aren't
 # set up on Hyprland.
+_: {
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
 
-{ pkgs, ... }:
-{
-	home.file = {
-		".config/user-dirs.dirs".text = ''
-			XDG_DESKTOP_DIR="$HOME/Desktop"
-			XDG_DOCUMENTS_DIR="$HOME/Documents"
-			XDG_DOWNLOAD_DIR="$HOME/Downloads"
-			XDG_MUSIC_DIR="$HOME/Music"
-			XDG_PICTURES_DIR="$HOME/Pictures"
-			XDG_PUBLICSHARE_DIR="$HOME/Public"
-			XDG_TEMPLATES_DIR="$HOME/Templates"
-			XDG_VIDEOS_DIR="$HOME/Videos"
-		'';
-	};
+      desktop = "$HOME/Desktop";
+      documents = "$HOME/Documents";
+      download = "$HOME/Downloads";
+      music = "$HOME/Music";
+      pictures = "$HOME/Pictures";
+      videos = "$HOME/Videos";
+
+      publicShare = "$HOME/.public";
+      templates = "$HOME/.templates";
+    };
+  };
 }
