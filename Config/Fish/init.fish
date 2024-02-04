9bcc5b2de0ac3b14f8c897a9898c0d1f92fed98e
Nowaaru <nowaaru@proton.me>
2024-02-04 16:52:08 -0700

    goodbye

--- Config/Fish/init.fish
+++ Config/Fish/init.fish
@@ -5,12 +5,5 @@ set -Ux MODULES $DIARY/Modules
 set -Ux USER_MODULES $MODULES/User
 set -Ux SYSTEM_MODULES $MODULES/System
 
-# Initialize abbreviations.
-abbr -a --position command mod "cd $MODULES"
-abbr -a --position command conf "cd $CONFIG"
-
-# Aliases.
-source $CONFIG/Fish/alias.fish
-
 # Git.
 source $CONFIG/Fish/git.fish
