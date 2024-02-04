124ca1557becee69b3fd681b24827d28e1ad0299
Nowaaru <nowaaru@proton.me>
2024-02-03 22:48:31 -0700

    kill vinegar
    its dead

--- Modules/User/Programming/roblox.nix
+++ Modules/User/Programming/roblox.nix
@@ -1,23 +1,21 @@
-{ pkgs, ... }:
-{
-    
-	home.file = {
-		".config/vinegar/config.toml".text = ''
-        sanitize_env = true
+{pkgs, ...}: {
+  home.file = {
+    ".config/vinegar/config.toml".text = ''
+            sanitize_env = true
 
-        [env]
-        WINEESYNC = "1"
-		[splash]
-		style = "familiar"
-        [studio]
-        dxvk = false
-        renderer = "D3D11FL10"
-        launcher = "gamemoderun"
-		'';
-	};
+            [env]
+            WINEESYNC = "1"
+      [splash]
+      style = "familiar"
+            [studio]
+            dxvk = false
+            renderer = "D3D11FL10"
+            launcher = "gamemoderun"
+    '';
+  };
 
-	home.packages = with pkgs; [
-		vinegar
-        rojo
-	];
+  home.packages = with pkgs; [
+    # vinegar
+    rojo
+  ];
 }
