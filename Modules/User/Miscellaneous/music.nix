1a2358bf94bd58e9f9039ffd30b32b3789fa74fc
Nowaaru <nowaaru@proton.me>
2024-02-04 18:50:37 -0700

    dear diary, today I realized that my spotify-player was fricked!
    so, i swapped it out with ncspot.
    i liked spotify-player more, though. :/

--- Modules/User/Miscellaneous/music.nix
+++ Modules/User/Miscellaneous/music.nix
@@ -1,8 +1,7 @@
-{ pkgs, ... }:
-{
-    home.packages = with pkgs; [
-        spotify-player
-        pulsemixer
-        catnip
-    ];
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    pulsemixer
+    ncspot
+    catnip
+  ];
 }
