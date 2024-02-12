76f8b7956a07a7a7e9f833c7ac55cecb3c97bdce
Nowaaru <nowaaru@proton.me>
2024-02-12 00:11:54 -0700

    fmt
    yep

--- Modules/User/Miscellaneous/Terminal/init.nix
+++ Modules/User/Miscellaneous/Terminal/init.nix
@@ -5,15 +5,17 @@
     ];
 
 	home.packages = with pkgs; [
-		neofetch
-		notcurses
-		ranger
-		tldr
-		page
-		glow
-	];
         asciinema-agg
         asciinema
+
+        notcurses
+        neofetch
+        ranger
+        tldr
+        page
+        glow
+
+    ];
 
     programs.starship = {
         settings = import ./starship.nix { inherit pkgs; };
