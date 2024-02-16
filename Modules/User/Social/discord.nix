8e3e7300ff1c80cec9fd52509201f56f1e36f361
Nowaaru <nowaaru@proton.me>
2024-02-15 19:07:03 -0700

    fmt + arrpc

--- Modules/User/Social/discord.nix
+++ Modules/User/Social/discord.nix
@@ -1,17 +1,14 @@
-{ pkgs, ...}:
-{
-	home.packages = with pkgs; 
-	let
-		discord-wayland = pkgs.discord.overrideAttrs ( old: rec {
-			commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
-	  	});
-		obsidian-wayland = pkgs.obsidian.overrideAttrs ( old: rec {
-			commandLineArgs = "--disable-gpu=true";
-	  	});
-	in
-	[
-	  # discord-wayland
-	  vesktop
-	  obsidian-wayland
-	];
+{pkgs, ...}: {
+  home.packages = with pkgs; let
+    discord-wayland = pkgs.discord.overrideAttrs (old: rec {
+      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+    });
+    obsidian-wayland = pkgs.obsidian.overrideAttrs (old: rec {
+      commandLineArgs = "--disable-gpu=true";
+    });
+  in [
+    obsidian-wayland
+    vesktop
+    arrpc
+  ];
 }
