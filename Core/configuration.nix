69f4de2887f3f03d5541f340586caddc9b629a60
Nowaaru <nowaaru@proton.me>
2024-02-19 22:09:26 -0700

    fmt

--- Core/configuration.nix
+++ Core/configuration.nix
@@ -89,85 +89,96 @@ in {
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
+  hardware = {
+    # Hardware setup.
+    nvidia = {
+      modesetting.enable = true; # required
+      powerManagement.enable = true; # can cause sleep settings but supposedly fixed with the newest nvidia update
+      powerManagement.finegrained = false; # experimental?
 
-  # Hardware setup.
-  hardware.nvidia = {
-    modesetting.enable = true; # required
-    powerManagement.enable = true; # can cause sleep settings but supposedly fixed with the newest nvidia update
-    powerManagement.finegrained = false; # experimental?
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
 
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+
+      extraPackages = with pkgs; [
+        intel-compute-runtime
+      ];
+    };
+
+    # Enable OpenTabletDriver.
+    opentabletdriver.enable = true;
+    pulseaudio.enable = false;
   };
+  services = {
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+      videoDrivers = ["nvidia"];
 
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+      displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
 
-    extraPackages = with pkgs; [
-      intel-compute-runtime
-    ];
-  };
+      # Enable the Cinnamon and Hypr for X11.
+      desktopManager.cinnamon.enable = true;
+      windowManager.hypr.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
+      # Configure keymap in X11
 
-  services.xserver.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
+      xkb.layout = "us";
+      xkb.variant = "";
+    };
 
-  # Enable the Cinnamon and Hypr for X11.
-  services.xserver.desktopManager.cinnamon.enable = true;
-  services.xserver.windowManager.hypr.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-  };
-
-  # Configure keymap in Wayland.
-  services.keyd = {
-    enable = true;
-    keyboards = {
-      apex7 = {
-        ids = ["1038:1618"];
-        settings = {
-          main = {
-            "volumeup up" = "mwheelup";
-            "volumedown up" = "mwheeldown";
+    # Configure keymap in Wayland.
+    keyd = {
+      enable = true;
+      keyboards = {
+        apex7 = {
+          ids = ["1038:1618"];
+          settings = {
+            main = {
+              "volumeup up" = "mwheelup";
+              "volumedown up" = "mwheeldown";
+            };
           };
         };
       };
     };
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+
+    # Enable Flatpak sandboxing.
+    flatpak.enable = true;
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable OpenTabletDriver.
-  hardware.opentabletdriver.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -187,63 +198,64 @@ in {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Session variables for gaming/gamescope.
-  environment.sessionVariables = {
-    # Hyprland!
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIX_OZONE_WL = "1";
-
-    # Gamescope.
-    WLR_RENDERER = "vulkan";
-    __GL_GSYNC_ALLOWED = "1";
-    __GL_VRR_ALLOWED = "0";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    LIBVA_DRIVER_NAME = "nvidia";
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";
-    PROTON_ENABLE_NGX_UPDATER = "1";
-    WLR_USE_LIBINPUT = "1";
-    ENABLE_VKBASALT = "1";
-    GBM_BACKEND = "nvidia-drm";
-  };
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    home-manager
-    libsForQt5.kio-admin
-
-    kitty
-    kitty-img
-
-    grc
-    fzf
-    zip
-    unzip
-
-    fishPlugins.done
-    fishPlugins.fzf-fish
-    fishPlugins.forgit
-    fishPlugins.hydro
-    fishPlugins.grc
-
-    htop
-    nvtop
-
-    dotnet-runtime
-
-    (nerdfonts.override {fonts = ["JetBrainsMono" "FiraMono" "FiraCode" "SpaceMono"];})
-    #	wget
-  ];
-
   # Hyprland!
   programs.hyprland = {
     enable = true;
     portalPackage = inputs.xdg-desktop-portal-hyprland.packages."${pkgs.system}".xdg-desktop-portal-hyprland;
   };
+  environment = {
+    # Session variables for gaming/gamescope.
+    sessionVariables = {
+      # Hyprland!
+      WLR_NO_HARDWARE_CURSORS = "1";
+      NIX_OZONE_WL = "1";
 
-  # Remove unnecessary packages from Plasma 5.
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-    konsole
-  ];
+      # Gamescope.
+      WLR_RENDERER = "vulkan";
+      __GL_GSYNC_ALLOWED = "1";
+      __GL_VRR_ALLOWED = "0";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      LIBVA_DRIVER_NAME = "nvidia";
+      WLR_RENDERER_ALLOW_SOFTWARE = "1";
+      PROTON_ENABLE_NGX_UPDATER = "1";
+      WLR_USE_LIBINPUT = "1";
+      ENABLE_VKBASALT = "1";
+      GBM_BACKEND = "nvidia-drm";
+    };
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    systemPackages = with pkgs; [
+      home-manager
+      libsForQt5.kio-admin
+
+      kitty
+      kitty-img
+
+      grc
+      fzf
+      zip
+      unzip
+
+      fishPlugins.done
+      fishPlugins.fzf-fish
+      fishPlugins.forgit
+      fishPlugins.hydro
+      fishPlugins.grc
+
+      htop
+      nvtop
+
+      dotnet-runtime
+
+      (nerdfonts.override {fonts = ["JetBrainsMono" "FiraMono" "FiraCode" "SpaceMono"];})
+      #	wget
+    ];
+
+    # Remove unnecessary packages from Plasma 5.
+    plasma5.excludePackages = with pkgs.libsForQt5; [
+      konsole
+    ];
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -252,14 +264,6 @@ in {
     enable = true;
     enableSSHSupport = true;
   };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Enable Flatpak sandboxing.
-  services.flatpak.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
