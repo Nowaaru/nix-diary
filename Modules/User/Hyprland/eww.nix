2f055f121fc5f3ee50331506fd91dd0a14c87bc5
Nowaaru <nowaaru@proton.me>
2024-03-07 13:47:32 -0700

    eww is fixed

--- Modules/User/Hyprland/eww.nix
+++ Modules/User/Hyprland/eww.nix
@@ -1,25 +1,25 @@
 { pkgs, ... }: {
     home.packages = with pkgs; [
-        eww-wayland
+        eww
     ];
 
-    nixpkgs = {
-        overlays = 
-            [
-                (final: prev:
-                    {
-                        eww = 
-                            prev.eww-wayland.overrideAttrs (old: rec {
-                                    src = pkgs.fetchFromGitHub {
-                                      owner = "elkowar";
-                                      repo = "eww";
-                                      rev = "65d622c81f2e753f462d23121fa1939b0a84a3e0";
-                                      hash = "sha256-MR91Ytt9Jf63dshn7LX64LWAVygbZgQYkcTIKhfVNXI=";
-                                    };
-                            });
-
-                        eww-wayland = final.eww.override { withWayland = true; };
-                    })
-            ];
-    };
+    # nixpkgs = {
+    #     overlays = 
+    #         [
+    #             (final: prev:
+    #                 {
+    #                     eww = 
+    #                         prev.eww-wayland.overrideAttrs (old: rec {
+    #                                 src = pkgs.fetchFromGitHub {
+    #                                   owner = "elkowar";
+    #                                   repo = "eww";
+    #                                   rev = "65d622c81f2e753f462d23121fa1939b0a84a3e0";
+    #                                   hash = "sha256-MR91Ytt9Jf63dshn7LX64LWAVygbZgQYkcTIKhfVNXI=";
+    #                                 };
+    #                         });
+    #
+    #                     eww-wayland = final.eww.override { withWayland = true; };
+    #                 })
+    #         ];
+    # };
 }
