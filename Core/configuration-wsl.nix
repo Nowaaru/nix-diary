dd0a2c61b7d0cfdc1484147f962c3b78cf2c48f7
Nowaaru <nowaaru@proton.me>
2024-03-12 03:20:18 +0000

    fmt + add fish plugins

--- Core/configuration-wsl.nix
+++ Core/configuration-wsl.nix
@@ -1,13 +1,29 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
-
-{ inputs, config, lib, pkgs, ... }:
-
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib; {
+  home.packages = [
+    grc
+    fzf
+    zip
+    unzip
+
+    fishPlugins.done
+    fishPlugins.fzf-fish
+    fishPlugins.forgit
+    fishPlugins.hydro
+    fishPlugins.grc
+  ];
+
   imports = [
     # include NixOS-WSL modules
     inputs.nixos-wsl.nixosModules.default
@@ -20,7 +36,7 @@ with lib; {
     defaultUser = mkForce "vert";
     useWindowsDriver = true;
     startMenuLaunchers = true;
-    
+
     interop.register = true;
 
     wslConf = {
@@ -33,10 +49,11 @@ with lib; {
     openssh.enable = true;
   };
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];  
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  users.defaultUserShell = pkgs.fish;
+  time.hardwareClockInLocalTime = false;
   programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
