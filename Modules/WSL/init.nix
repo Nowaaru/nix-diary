51d3487a528d15be0aa974a29e7ef938d8682bd8
Nowaaru <nowaaru@proton.me>
2024-03-12 02:33:31 +0000

    add fish to wsl

--- Modules/WSL/init.nix
+++ Modules/WSL/init.nix
@@ -7,18 +7,36 @@
   imports = [
     inputs.neovim-flake.homeManagerModules.default
     ./Git/init.nix
+    ./Terminal/init.nix
   ];
 
   home.packages = with pkgs; [
     lazygit
   ];
 
+  home.shellAliases = {
+    ls = "${pkgs.lsd}/bin/lsd";
+  };
   programs = {
     neovim-flake = {
       enable = true;
       settings = import (inputs.self + /Config/Neovim) {
         inherit pkgs lib inputs;
       }; # bruh?
+    };
+
+    fish = {
+      enable = true;
+      # useBabelfish = true;
+      shellInit = ''
+        set -Ux DIARY ~/.diary
+        source $DIARY/Config/Fish/init.fish
+      '';
+      interactiveShellInit = ''
+        set -U fish_greeting
+        ${pkgs.neofetch}/bin/neofetch
+        fish_vi_key_bindings
+      '';
     };
 
     home-manager.enable = true;
