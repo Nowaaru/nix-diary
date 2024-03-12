d468c32bcb412b40de7f430ebc4d8c9c41ae1d8d
Nowaaru <nowaaru@proton.me>
2024-03-11 22:05:44 -0700

    emulator

--- Modules/System/qemu.nix
+++ Modules/System/qemu.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
-  environment.systemmPackages = [
+  virtualisation.libvirtd.enable = true;
+  users.groups.libvirtd.members = ["root" "noire"];
+
+  environment.systemPackages = [
     pkgs.qemu
+    pkgs.quickemu
     (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
       qemu-system-x86_64 \
         -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
