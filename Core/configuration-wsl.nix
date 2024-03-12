ee65fd39e0612b42442510b6903270defea08365
Nowaaru <nowaaru@proton.me>
2024-03-12 02:33:08 +0000

    formatting
    yep.

--- Core/configuration-wsl.nix
+++ Core/configuration-wsl.nix
@@ -35,6 +35,9 @@ with lib; {
 
   nix.settings.experimental-features = ["nix-command" "flakes"];  
 
+  users.defaultUserShell = pkgs.fish;
+  programs.fish.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
--- Modules/WSL/init.nix
+++ Modules/WSL/init.nix
@@ -9,16 +9,18 @@
     ./Git/init.nix
   ];
 
-  programs.neovim-flake = {
-    enable = true;
-    settings = import (inputs.self + /Config/Neovim) {
-      inherit pkgs lib inputs;
-    }; # bruh?
-  };
-
   home.packages = with pkgs; [
     lazygit
   ];
 
-  programs.home-manager.enable = true;
+  programs = {
+    neovim-flake = {
+      enable = true;
+      settings = import (inputs.self + /Config/Neovim) {
+        inherit pkgs lib inputs;
+      }; # bruh?
+    };
+
+    home-manager.enable = true;
+  };
 }
