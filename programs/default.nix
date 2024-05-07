ee59ceccd0c340577154826e1a59e73e3d097e58
Nowaaru <nowaaru@proton.me>
2024-05-07 13:15:56 -0700

    completely gut roblox

---
+++ programs/default.nix
@@ -0,0 +1,30 @@
+{lib, ...} @ args: let
+  inherit (lib) strings attrsets;
+
+  directoryPredicate = dir:
+    attrsets.foldlAttrs (
+      acc: filename: type:
+        acc
+        // {
+          ${(strings.removeSuffix ".nix" filename)} =
+            if type == "directory"
+            then (directoryPredicate "${dir}/${filename}")
+            else
+              (mergePredicate dir (
+                  if filename == "default"
+                  then "all"
+                  else filename
+                )
+                type);
+        }
+    ) {} (attrsets.filterAttrs (filename: type: type == "directory" || filename != "default.nix") (builtins.readDir dir));
+
+  mergePredicate = dir: k: v: (
+    if v == "directory"
+    # TODO: if its a directory
+    # then run mergepredicate on the builtin files
+    then directoryPredicate "${dir}/${k}"
+    else (import "${dir}/${k}" args)
+  );
+in
+  directoryPredicate ./.
--- usr/dev/roblox/default.nix
+++ usr/dev/roblox/default.nix
@@ -1,10 +1,4 @@
-{
-  stdenv,
-  pkgs,
-  ...
-}: let
-  selfTrace = w: builtins.trace w w;
-in {
+{pkgs, ...}: {
   home = {
     packages = with pkgs; [
       (writeShellScriptBin "vinegar" ''
@@ -26,38 +20,5 @@ in {
         renderer = "Vulkan"
       '';
     };
-
-    /*
-      nixpkgs.overlays = [
-      (final: prev: {
-        vinegar = prev.vinegar.overrideAttrs (old:
-          with lib.lists; rec {
-            version = "1.7.3";
-            src = prev.fetchFromGitHub {
-              owner = "vinegarhq";
-              repo = "vinegar";
-              rev = "v${version}";
-              hash = "sha256-aKL+4jw/uMbbvLRCBHstCTrcQ1PTYSCwMNgXTvSvMeY=";
-            };
-            buildInputs = forEach (old.buildInputs) (
-              idx:
-                if (lib.strings.hasInfix "wine" idx.pname)
-                then
-                  prev.wineWowPackages.stagingFull.overrideDerivation (oldAttrs: {
-                    patches =
-                       (oldAttrs.patches or []) ++  [
-                        # (pkgs.fetchurl {
-                        #   name = "vinegar-wine-segrevert.patch";
-                        #   url = "https://raw.githubusercontent.com/flathub/org.vinegarhq.Vinegar/e24cb9dfa996bcfeaa46504c0375660fe271148d/patches/wine/segregrevert.patch";
-                        #   hash = "sha256-MzX17s8GtdeHqg+WsfDEQn1A0XeBTEwQf/WilUxkkTU=";
-                        # })
-                      ];
-                  })
-                else idx
-            );
-          });
-      })
-    ];
-    */
   };
 }
