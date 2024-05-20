37f339984411df42a5861161560bfd48f40212c4
Nowaaru <nowaaru@proton.me>
2024-05-20 10:35:35 -0700

    ffmt

--- sys/conf/register-users.nix
+++ sys/conf/register-users.nix
@@ -4,13 +4,21 @@
   lib,
   ...
 }: {
-  users.users =
-    builtins.mapAttrs (k: _: {
+  users.users = builtins.mapAttrs (k: _:
+    {
       isNormalUser = true;
       description = "user - ${k}";
       shell = pkgs.fish;
       extraGroups = ["networkmanager" "wheel" "libvirtd"];
       packages = []; # managed via home-manager
-    })
-    (lib.attrsets.filterAttrs (_: v: v == "directory") (builtins.readDir (inputs.self + /usr)));
+    }
+    // (
+      let
+        path = inputs.self + /usr/${k}/meta.nix;
+      in
+        if (builtins.pathExists path)
+        then (import path)
+        else {}
+    ))
+  (lib.attrsets.filterAttrs (_: v: v == "directory") (builtins.readDir (inputs.self + /usr)));
 }
