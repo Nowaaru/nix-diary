e13387c9cc7922a179e8bed403af185b9f058ec8
Nowaaru <nowaaru@proton.me>
2024-06-12 19:10:35 -0700

    add floorp desktop entry

--- usr/noire/programs/floorp/default.nix
+++ usr/noire/programs/floorp/default.nix
@@ -1,24 +1,28 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: let
+  floorpPath = "${pkgs.floorp-unwrapped}/bin/floorp";
+in {
+  xdg.desktopEntries.floorp = {
+    name = "Floorp";
+    actions = {
+      "new-window" = {
+        exec = "${floorpPath} --new-instance %U";
+      };
+
+      "new-private-window" = {
+        exec = "${floorpPath} --private-window %U";
+      };
+    };
+    categories = ["Network" "WebBrowser"];
+    comment = "A beautiful, customizable gecko-based Japanese browser.";
+    exec = "${floorpPath} --name floorp %U";
+    genericName = "Web Browser";
+    startupNotify = true;
+    type = "Application";
+    icon = "${pkgs.floorp-unwrapped}/lib/floorp/browser/chrome/icons/default/default128.png";
+    mimeType = ["text/html" "text/xml" "application/xhtml+xml" "application/vnd.mozilla.xul+xml" "x-scheme-handler/http" "x-scheme-handler/https"];
+  };
+
   home.packages = with pkgs; [
     floorp-unwrapped
-    (pkgs.writeTextDir "share/applications/floorp.desktop" ''
-      [Desktop Entry]
-      Name=Floorp
-      Actions=new-private-window;new-window;profile-manager-window
-      Comment=A beautiful, customizable gecko-based Japanese browser.
-      MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https
-      Exec=floorp --name floorp %U
-      Terminal=false
-      Icon=${pkgs.floorp-unwrapped.out + "/lib/floorp/browser/chrome/icons/default/default128.png"}
-      Type=Application
-      Categories=Network;WebBrowser
-      StartupNotify=true
-      GenericName=Web Browser
-    '')
   ];
 }
