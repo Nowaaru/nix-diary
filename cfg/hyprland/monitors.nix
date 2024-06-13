53b0fb2f8a9ec9ef9b49a041d88671facc975d89
Nowaaru <nowaaru@proton.me>
2024-06-13 14:12:25 -0700

    re-add monitors to hyprland

--- cfg/hyprland/monitors.nix
+++ cfg/hyprland/monitors.nix
@@ -1,6 +1,6 @@
 {
-    monitor = [
-        "DP-3,1920x1080@165,0x0,auto,vrr,1,bitdepth,8"
-        "HDMI-A-1,1920x1080@75,-1920x0,auto,vrr,1"
-    ];
+  monitor = [
+    "DP-1,1920x1080@165,0x0,auto,vrr,1,bitdepth,8"
+    "DP-2,1920x1080@75,-1920x0,auto,vrr,1"
+  ];
 }
