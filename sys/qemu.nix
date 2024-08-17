55f0cea7f37a16db566f90ae000033a57f58caaf
Nowaaru <nowaaru@proton.me>
2024-08-16 20:32:09 -0700

    goodbye qemu (python sucks)

--- sys/qemu.nix
+++ sys/qemu.nix
@@ -2,13 +2,13 @@
   virtualisation.libvirtd.enable = true;
   users.groups.libvirt.members = ["root" "noire"];
 
-  environment.systemPackages = [
-    pkgs.qemu
-    pkgs.quickemu
-    (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
-      qemu-system-x86_64 \
-        -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-        "$@"
-    '')
-  ];
+  # environment.systemPackages = [
+  #   pkgs.qemu
+  #   pkgs.quickemu
+  #   (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+  #     qemu-system-x86_64 \
+  #       -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+  #       "$@"
+  #   '')
+  # ];
 }
