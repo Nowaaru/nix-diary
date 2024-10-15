8110c00629515ab06d07f972b510ceb2dd24ac1e
Nowaaru <nowaaru@proton.me>
2024-10-15 15:47:57 -0700

    change kitty font
    for some reason it was really jacked and some ligatures
    were really bad for what's supposed to be a monospaced
    font sent ergh...

--- usr/noire/cfg/kitty.nix
+++ usr/noire/cfg/kitty.nix
@@ -1,9 +1,20 @@
-{pkgs}: {
-  theme = "kanagawabones";
+{ pkgs, lib }: 
+let
+  fontName = "VictorMonoNFM-Semibold";
+in 
+{
+  themeFile = "kanagawabones";
   shellIntegration.enableFishIntegration = true;
 
+  settings = {
+    disable_ligatures = "cursor";
+    font_family = lib.mkDefault fontName;
+    font_features = "${ fontName } +liga";
+  };
+
   font = {
-    name = "SpaceMono";
-    package = pkgs.nerdfonts.override {fonts = ["SpaceMono"];};
+    name = lib.mkForce fontName;
+    package = lib.mkForce pkgs.nerdfonts; # (pkgs.nerdfonts.override {fonts = [ fontName ];});
   };
 }
+
