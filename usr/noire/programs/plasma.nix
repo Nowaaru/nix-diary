d81d979a326db250dadcbcd1f58801699140e7c7
Nowaaru <nowaaru@proton.me>
2024-12-02 14:16:01 -0700

    enable xdg portal for plasma

--- usr/noire/programs/plasma.nix
+++ usr/noire/programs/plasma.nix
@@ -1,6 +1,8 @@
-{ inputs, configure, ... }: 
+{ inputs, pkgs, configure, ... }: 
 {
   imports = [inputs.plasma-manager.homeManagerModules.plasma-manager];
   
   programs.plasma = configure "plasma";
+
+  xdg.portal.enable = true; 
 }
