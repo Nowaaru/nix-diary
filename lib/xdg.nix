82ee63e8f067689bd919ff922e4ba736abac9df0
Nowaaru <nowaaru@proton.me>
2024-12-02 14:12:54 -0700

    make generic 'mkShortcut' function for xdg

---
+++ lib/xdg.nix
@@ -0,0 +1,35 @@
+_: {
+  mkShortcut = {
+    pkg ? null,
+    executable ? pkg + "/bin/${pkg.pname}",
+    instance-name ? pkg.pname,
+    display-name ? "${instance-name} - Shortcut",
+    shortcut-comment ? "A shortcut for ${instance-name}.",
+    # https://specifications.freedesktop.org/menu-spec/1.0/category-registry.html
+    categories ? [],
+    actions ? {},
+
+    icon-path ? "${pkg}/lib/${pkg.pname}/browser/chrome/icons/default/default128.png",
+    icon ? icon-path,
+
+    mimeTypes ? [],
+    mimeType ? [],
+    genericName ? "Unknown",
+    type ? "Application",
+
+    startupNotify ? false,
+
+    terminal ? false,
+    useTerminal ? terminal,
+    extraSettings ? {},
+    settings ? extraSettings,
+  }: {
+      inherit categories genericName startupNotify actions type icon settings;
+      name = display-name;
+      comment = "${shortcut-comment}";
+      # https://specifications.freedesktop.org/desktop-entry-spec/latest/exec-variables.html
+      exec = "${executable}";
+      mimeType = mimeType ++ mimeTypes;
+      terminal = terminal && useTerminal;
+  };
+}
