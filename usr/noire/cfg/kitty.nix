025154a40d9f61747bf8f84697cd78e906a887f6
Nowaaru <nowaaru@proton.me>
2024-12-02 14:15:42 -0700

    update kitty config

--- usr/noire/cfg/kitty.nix
+++ usr/noire/cfg/kitty.nix
@@ -10,6 +10,23 @@ in
     disable_ligatures = "cursor";
     font_family = lib.mkDefault fontName;
     font_features = "${ fontName } +liga";
+
+    # this is really annoying
+    confirm_os_window_close = 0;
+
+    # cursor_trail_decay = "[0.1 0.4]";
+    cursor_trail_start_threshold = 2;
+    cursor_trail = 1;
+
+    strip_trailing_spaces = "smart";
+
+    # Repaint delay in MS, 10 = 10ms per frame = 100fps
+    # 6ms = 165fps
+    repaint_delay = 6;
+    # flickering is not a problem anymore whooo!
+    sync_to_monitor = "no";
+
+    bell_path = "none";
   };
 
   font = {
