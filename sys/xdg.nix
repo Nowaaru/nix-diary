bb6389953513eb5e716349053ea7b8cf93f4c6e2
Nowaaru <nowaaru@proton.me>
2024-12-02 14:13:58 -0700

    add system xdg items

---
+++ sys/xdg.nix
@@ -0,0 +1,25 @@
+{
+  xdg = {
+    terminal-exec = {
+      enable = true;
+
+      settings = {
+        default = [
+          "kitty.desktop"
+        ];
+
+        Hyprland = [
+          "kitty.desktop"
+        ];
+      };
+    };
+
+
+    mime.enable = true;
+    icons.enable = true;
+    menus.enable = true;
+    sounds.enable = true;
+    autostart.enable = true;
+    portal.wlr.enable = true;
+  };
+}
