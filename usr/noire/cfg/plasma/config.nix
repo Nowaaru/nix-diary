d0929b83f697a9f8743a35fedefe0800790237c2
Nowaaru <nowaaru@proton.me>
2024-12-12 09:26:46 -0700

    add qt quick render settings
    see:
    https://discuss.kde.org/t/panel-icons-disappear-after-upgrade-to-kde-6/24352/2?u=nowaaru

--- usr/noire/cfg/plasma/config.nix
+++ usr/noire/cfg/plasma/config.nix
@@ -1,3 +1,9 @@
 {
   ksmserverrc.General.loginMode.value = "emptySession";
+
+  # see: https://discuss.kde.org/t/panel-icons-disappear-after-upgrade-to-kde-6/24352/2?u=nowaaru
+  kglobals.QtQuickRendererSettings = {
+    RenderLoop = "basic";
+    SceneGraphBackend = "opengl";
+  };
 }
