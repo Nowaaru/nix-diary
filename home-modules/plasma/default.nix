3388cd1d4cc58857d8dbaa74296ed0d23266ce72
Nowaaru <nowaaru@proton.me>
2024-12-29 21:52:19 -0700

    add theme entries to plasma module

--- home-modules/plasma/default.nix
+++ home-modules/plasma/default.nix
@@ -1,25 +1,56 @@
-{ config, lib, ... }:
-let
+{
+  config,
+  lib,
+  ...
+}: let
   cfg = config.programs.plasma;
-in with lib; {
-  imports = [];
+in
+  with lib; {
+    imports = [];
 
-  options.programs.plasma = with types; {
-    globalThemes = mkOption {
-        type = attrsOf (oneOf [ package string ]);
+    options.programs.plasma = with types; {
+      globalThemes = mkOption {
+        type = attrsOf (oneOf [package string]);
         description = "attribute set of packages to be extracted into '~/.local/share/plasma'.";
         default = {};
-    };
-  };
+      };
 
-  config = mkIf cfg.enable {
-      home.file = if (builtins.hasAttr "globalThemes" cfg) then ((foldlAttrs (a: k: v: a // {
-        ${k} = {
-          enable = true;
-          recursive = true;
-          target = ".local/share/plasma";
-          source = builtins.toString v;
-        };
-      })) {} cfg.globalThemes) else {};
-  };
-}
+      iconThemes = mkOption {
+        type = attrsOf (oneOf [package string]);
+        description = "attribute set of packages to be extracted into '~/.local/share/icons'.";
+        default = {};
+      };
+
+      cursorThemes = mkOption {
+        type = attrsOf (oneOf [package string]);
+        description = "attribute set of packages to be extracted into '~/.local/icons'.";
+        default = {};
+      };
+    };
+
+    config = mkIf cfg.enable {
+      home.file =
+        (foldlAttrs (a: k: v:
+          a
+          // {
+            "plasma-icon-theme-${k}" = {
+              enable = true;
+              recursive = true;
+              target = ".local/share/icons";
+              source = builtins.toString v;
+            };
+          }) {}
+        cfg.iconThemes)
+        // (foldlAttrs (a: k: v:
+          a
+          // {
+            "plasma-global-theme-${k}" = {
+              enable = true;
+              recursive = true;
+              target = ".local/share/plasma";
+              source = builtins.toString v;
+            };
+          }) {}
+        cfg.globalThemes);
+    };
+  }
