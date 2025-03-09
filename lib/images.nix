02bd029b9a1eaa2f04ba1e5f384db46a103b2218
Nowaaru <nowaaru@proton.me>
2025-03-09 00:27:09 -0700

    update image library

--- lib/images.nix
+++ lib/images.nix
@@ -3,9 +3,9 @@
   lib,
   ...
 }: {
-  gifToImages = imagePath:
+  resizeImage = resize: imagePath:
     pkgs.stdenv.mkDerivation (finalAttrs: {
-      pname = "gif-to-image-result";
+      pname = "resize-image-result";
       version = "1.0.0";
 
       unpackPhase = ''true'';
@@ -13,7 +13,7 @@
       src = with lib;
         (lib.evalModules {
           modules = with lib.types;
-          with (import (./types.nix) lib); [
+          with (import ./types.nix lib); [
             {
               options = {
                 image = mkOption {
@@ -44,7 +44,59 @@
 
       buildPhase = ''
         mkdir -p $out;
-        magick $src -coalesce $out/%d.png
+        magick $src -resize ${resize} $out/%d.png
+      '';
+    });
+
+  gifToImages = {
+    imagePath,
+    resize ? "1280x720",
+  } @ params:
+    pkgs.stdenv.mkDerivation (finalAttrs: {
+      pname = "gif-to-image-result";
+      version = "1.0.0";
+
+      unpackPhase = ''true'';
+
+      src = with lib;
+        (lib.evalModules {
+          modules = with lib.types;
+          with (import ./types.nix lib); [
+            {
+              options = {
+                image = mkOption {
+                  type = let
+                    imageUnionType = oneOf [
+                      (imageWithExt ".gif")
+                      (imageWithExt ".png")
+                      (imageWithExt ".jpg")
+                      (imageWithExt ".jpeg")
+                    ];
+                  in
+                    either imageUnionType (listOf imageUnionType);
+
+                  description = "the image, or list of images to use for the theme";
+                };
+              };
+
+              config.image = imagePath;
+            }
+          ];
+        })
+        .config
+        .image;
+
+      nativeBuildInputs = with pkgs; [
+        imagemagick
+      ];
+
+      buildPhase = ''
+        mkdir -p $out;
+        magick $src -coalesce${
+          if resize
+          then " -resize ${resize} "
+          else ""
+        }$out/%d.png
       '';
     });
 }
