55d88d5180ca5397124fdae8c4cc4c325944141a
Nowaaru <nowaaru@proton.me>
2025-03-30 14:42:54 -0700

    disable suspend/hibernate

--- systems/lastation/conf/sleep.nix
+++ systems/lastation/conf/sleep.nix
@@ -1,8 +1,8 @@
 _: {
   systemd.targets = {
     sleep.enable = true;
-    suspend.enable = true;
-    hibernate.enable = true;
+    suspend.enable = false; # this shit makes my pc un-wakeable after some time and i have to FULLY switch that mf off so nah
+    hibernate.enable = false;
     hybrid-sleep.enable = false;
   };
 }
