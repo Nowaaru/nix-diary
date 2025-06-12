4f22cee6605a09ede86f340cfc42ec33ac8a6dba
Nowaaru <nowaaru@proton.me>
2025-06-11 22:17:28 -0700

    update users

--- users/default.nix
+++ users/default.nix
@@ -9,23 +9,38 @@ args @ {
     inherit (inputs) nix-colors;
     inherit (args) inputs nur modules;
     inherit (lib) withSystem;
+
+    inherit (args.self.legacyPackages.x86_64-linux) stable;
+    # stable = lib.traceVal (builtins.attrNames args.self);
   };
 in
   with lib.gamindustri.users;
-    lib.traceVal (mkHomeManager [
+    (mkHomeManager [
       # me!
       (mkUser "noire" {
         system = "x86_64-linux";
+        extraSpecialArgs = {
+            inherit (args.self.legacyPackages.x86_64-linux) stable;
+        };
         sessionVariables = {
           EDITOR = "nvim";
         };
       })
 
       (mkUser "blanc" {
+        system = "aarch64-linux";
+        sessionVariables = {
+          EDITOR = "nvim";
+        };
+      })
+
+      (mkUser "sm1l3y4l" {
         system = "x86_64-linux";
         sessionVariables = {
           EDITOR = "nvim";
         };
+
+        extraSpecialArgs.stable = args.self.legacyPackages.x86_64-linux.stable;
       })
 
       # (mkUser "vert" {
