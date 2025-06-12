1fc93b908fb4ddbde863f90e07e0e0e7211cc85f
Nowaaru <nowaaru@proton.me>
2025-06-11 22:16:36 -0700

    update nmm mods

--- users/noire/programs/nmm.nix
+++ users/noire/programs/nmm.nix
@@ -1,11 +1,39 @@
-{inputs, ...}: let
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: let
   removeOverrides = k: builtins.removeAttrs k ["__functors" "override" "overrideDerivation" "__functionArgs"];
+  clients = removeOverrides (builtins.mapAttrs (_: removeOverrides) inputs.nmm-mods.clients);
 in {
-  imports = [inputs.nix-mod-manager.homeManagerModules.default];
+  imports = [
+    inputs.nix-mod-manager.homeManagerModules.default
+  ];
+
   programs.nix-mod-manager = {
     enable = true;
     forceGnuUnzip = true;
 
-    clients = removeOverrides (builtins.mapAttrs (_: removeOverrides) inputs.nmm-mods.clients);
+    inherit clients;
+  };
+
+  home.file.nmm-ggst-module-extra-costumes-patch-config = {
+    enable = true;
+    force = true;
+    text = lib.foldlAttrs (acc: k: v: acc + ''${k} = ${(
+          if (builtins.typeOf v) == "bool"
+          then lib.trivial.boolToString
+          else builtins.toString
+        )
+        v}''\n'') "" {
+      Enable_Console = true;
+      File_Access_Logging = false;
+      Loose_File_Loading = false;
+      Costumes_Patch = true;
+      Random_Costumes = false;
+      Log_Script_Errors = true;
+    };
+    target = lib.strings.normalizePath "${clients.guilty-gear-strive.rootPath}/${clients.guilty-gear-strive.binaryPath}/plugins/config.toml";
   };
 }
