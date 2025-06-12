eb2473c1ed5aeebbc2b69b2df9ab56f97f241246
Nowaaru <nowaaru@proton.me>
2025-06-11 22:18:06 -0700

    add pi as dependent machine

--- systems/lastation/sys/util/build.nix
+++ systems/lastation/sys/util/build.nix
@@ -1,6 +1,23 @@
-{ pkgs, ... }:
-{
-	environment.systemPackages = with pkgs; [
-		gcc
-	];
+{pkgs, ...}: {
+  # point at your Pi as an aarch64 builder
+  nix.buildMachines = [
+    {
+      hostName = "192.168.0.165"; # your Pi’s hostname or IP
+      sshUser = "blanc";
+
+      protocol = "ssh-ng";
+      system = "aarch64-linux";
+
+      maxJobs = 2; # Pi is slower—don’t swamp it!
+      speedFactor = 10; # weights scheduling
+
+      supportedFeatures = []; # leave empty for vanilla builds
+    }
+  ];
+
+  nix.distributedBuilds = true;
+
+  environment.systemPackages = with pkgs; [
+    gcc
+  ];
 }
